357b7f7240602e588a36672ad44017a9806d5555
kishore <kishore@sixtyonesteps.com>
2025-02-28 16:40:25 +0530

    Documents updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/admin-portal"
+            to="/docs/category/backend-API"
           >
             Start Now 🚀
           </Link>
